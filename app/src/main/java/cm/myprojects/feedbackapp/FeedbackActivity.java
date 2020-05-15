a628b1cca3ca95ab8835f5d5040ffaa70ec681ee
semyonmoroshek <s.moroseks@gmail.com>
2020-05-15 15:29:10 +0300

    3 - basic design

--- app/src/main/java/cm/myprojects/feedbackapp/FeedbackActivity.java
+++ app/src/main/java/cm/myprojects/feedbackapp/FeedbackActivity.java
@@ -2,6 +2,9 @@ package cm.myprojects.feedbackapp;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.RatingBar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +16,16 @@ public class FeedbackActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
 
+        EditText editReview = findViewById(R.id.et_review);
 
+    }
+
+    public void rootLayoutTapped(View view) {
+        try {
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
