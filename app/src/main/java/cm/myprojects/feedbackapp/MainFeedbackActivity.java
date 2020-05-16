9e63158f4b1768dfcc9a7f1e9fb2c89a2d27ad82
semyonmoroshek <s.moroseks@gmail.com>
2020-05-16 18:16:51 +0300

    5 - add feedback message

--- app/src/main/java/cm/myprojects/feedbackapp/MainFeedbackActivity.java
+++ app/src/main/java/cm/myprojects/feedbackapp/MainFeedbackActivity.java
@@ -1,27 +1,53 @@
 package cm.myprojects.feedbackapp;
 
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class FeedbackActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainFeedbackActivity extends AppCompatActivity {
 
     TextView txtReview;
+    Button btnSendFeedback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
 
+        setTitle("");
+
         txtReview = findViewById(R.id.txt_review);
+        btnSendFeedback = findViewById(R.id.btn_send_feedback_main);
 
         txtReview.setOnClickListener(View -> openReviewActivity());
+        btnSendFeedback.setOnClickListener(View -> sendFeedBack());
 
+    }
+
+    private void sendFeedBack() {
+
+        Dialog dialog = new Dialog(this);
+        dialog.setTitle("Thank you for feedback!");
+        dialog.setContentView(R.layout.my_dialog);
+        dialog.show();
+
+        final Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                dialog.dismiss();
+                dialog.cancel();
+            }
+        }, 5000);
     }
 
     private void openReviewActivity() {
--- app/src/main/java/cm/myprojects/feedbackapp/ReviewActivity.java
+++ app/src/main/java/cm/myprojects/feedbackapp/ReviewActivity.java
@@ -2,13 +2,52 @@ package cm.myprojects.feedbackapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ReviewActivity extends AppCompatActivity {
+
+    Button btnCancel, btnSendReview;
+    EditText editReview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+
+        btnCancel = findViewById(R.id.btn_cancel);
+        btnSendReview = findViewById(R.id.btn_send_review);
+        editReview = findViewById(R.id.et_message);
+
+        btnCancel.setOnClickListener(View -> returnToMainFeedbackActivity());
+        btnSendReview.setOnClickListener(View -> sendReview());
+    }
+
+    private void sendReview() {
+
+            Dialog dialog = new Dialog(this);
+            dialog.setTitle("Thank you for feedback!");
+            dialog.setContentView(R.layout.my_dialog);
+            dialog.show();
+
+            final Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    dialog.dismiss();
+                    dialog.cancel();
+                    finish();
+                }
+            }, 5000);
+        }
+
+    private void returnToMainFeedbackActivity() {
+        finish();
     }
 }
